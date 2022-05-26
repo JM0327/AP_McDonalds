824d86a1f3fb93599840c83686aeb40c2b46ac24
JM0327 <jiaming.huang@unil.ch>
2022-05-26 09:36:09 +0200

    version after fixing tkcanvas pro

---
+++ application/main.py
@@ -0,0 +1,15 @@
+from interface_1 import *
+
+"""
+initiate application
+"""
+
+
+class Run:
+    window = tk.Tk()
+    InfoPage(master=window)
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    Run()
